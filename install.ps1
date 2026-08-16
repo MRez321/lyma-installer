62c1cea96046d03d328fb6c2078f3482d0f48e78
Reza Mousavi <120497920+MRez321@users.noreply.github.com>
2026-08-16 13:17:14 +0330

    Update install.ps1 to install in current path

--- install.ps1
+++ install.ps1
@@ -1,5 +1,6 @@
 $ErrorActionPreference = "Stop"
-Write-Host "lyma-docker Local Installer"
+
+Write-Host "lyma-docker Installer"
 Write-Host "-------------------------------"
 
 Write-Host "Please enter your Nexus credentials in the popup window..."
@@ -12,17 +13,23 @@ $LatestUrl = "https://repo.lymagroups.ir/repository/lyma-raw-hosted/lyma-docker/
 $Version = (Invoke-RestMethod -Uri $LatestUrl -Headers $headers).Trim()
 $NexusUrl = "https://repo.lymagroups.ir/repository/lyma-raw-hosted/lyma-docker/$Version/lyma-docker-$Version.zip"
 
-$ActiveLink = ".\lyma-docker"
-$ZipPath = "$env:TEMP\lyma-docker-local.zip"
+$TargetDir = ".\lyma-docker"
+$TempDir = "$env:TEMP\lyma_extract"
+$ZipPath = "$env:TEMP\lyma.zip"
 
 Write-Host "Downloading v$Version to current directory..."
 Invoke-WebRequest -Uri $NexusUrl -Headers $headers -OutFile $ZipPath
 
-if (Test-Path $ActiveLink) { Remove-Item -Recurse -Force $ActiveLink }
-Expand-Archive -Path $ZipPath -DestinationPath ".\temp_extract" -Force
-Remove-Item $ZipPath
+Write-Host "Extracting..."
+if (Test-Path $TargetDir) { Remove-Item -Recurse -Force $TargetDir }
+New-Item -ItemType Directory -Force -Path $TargetDir | Out-Null
 
-Move-Item ".\temp_extract\lyma-docker-$Version" -Destination $ActiveLink
-Remove-Item ".\temp_extract" -Recurse -Force
+Expand-Archive -Path $ZipPath -DestinationPath $TempDir -Force
 
-Write-Host "Successfully extracted to $(Resolve-Path $ActiveLink)"
+# Move contents of the nested git archive folder directly into .\lyma-docker
+Move-Item "$TempDir\lyma-docker-$Version\*" -Destination $TargetDir -Force
+
+Remove-Item $ZipPath -Force
+Remove-Item $TempDir -Recurse -Force
+
+Write-Host "Successfully extracted to $(Resolve-Path $TargetDir)"
